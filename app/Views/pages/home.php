84ce7e899a30be453124dd0a1347db4d81021b56
wesleywil <wesleywilsonti@gmail.com>
2023-08-03 19:28:31 -0300

    added some basic views and controllers

---
+++ app/Views/pages/home.php
@@ -0,0 +1,7 @@
+<div class="h-[90vh] flex items-center justify-center">
+    <div class="text-center flex flex-col items-center justify-center">
+        <h1 class="text-6xl font-bold">Social Media Dashboard</h1>
+        <h2 class="text-2xl font-semibold">Stay Connected and Manage Your Social Accounts with Ease</h2>
+        <button class="mt-8 px-2 py-1 text-4xl text-white bg-red-600 hover:bg-red-800 rounded">SignIn</button>
+    </div>
+</div>
---
+++ app/Views/pages/login.php
@@ -0,0 +1,16 @@
+<div class="h-[90vh] flex flex-col items-center justify-center">
+    <h1 class="mb-4 text-2xl font-semibold">Login</h1>
+    <form class="p-2 flex flex-col items-center justify-center gap-2 text-xl bg-slate-200 rounded-xl">
+
+        <input type="text" placeholder="Username" class="w-full px-2 py-1  rounded">
+        <input type="password" placeholder="Password" class="w-full px-2 py-1  rounded">
+        <div class="mt-2 flex gap-4">
+            <button class="px-2 py-1 text-white bg-red-600 hover:bg-red-800 rounded">Submit</button>
+            <button class="px-2 py-1 text-white bg-red-600 hover:bg-red-800 rounded">Cancel</button>
+        </div>
+
+        <span class="mt-4 px-2 text-slate-600">Don't have an account yet? Click here and sign Up</span>
+
+
+    </form>
+</div>
